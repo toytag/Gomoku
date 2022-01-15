2d2f1b278632975046cf29d8c5f129de7342b6e0
Zhenzhong Tang <zztang@seas.upenn.edu>
2022-01-15 03:09:00 -0500

    update

--- gomoku-core/package/src/tests/MonteCarloTreeSearch.test.ts
+++
@@ -1,8 +0,0 @@
-import { MonteCarloTreeNode } from '../index';
-
-it("test", () => {
-  const root = new MonteCarloTreeNode;
-  const node = new MonteCarloTreeNode([7, 7], root);
-
-  node.mcts();
-});
